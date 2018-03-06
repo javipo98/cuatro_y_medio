2b627c560d19b90cb4822280fec89a514ba1c97b
m-matosflorimon <m.matosflorimon@gmail.com>
2018-03-05 23:00:26 -0400

    Abri documentos

--- cuatro_y_medio_pau.py
+++ cuatro_y_medio_pau.py
@@ -1,8 +1,23 @@
-with open("Excersises.csv") as Excersises:
-    excersises_list = []
+import os.path
+# with open("Excersises.csv") as Excersises:
+#     excersises_list = []
 
-with open("codewars.csv") as codewars:
-    codewars_list = []
-    if "Grasshopper" in codewars:
-        for lines in codewars:
-            print(lines)
+# with open("codewars.csv") as codewars:
+#     codewars_list = []
+#     if "Grasshopper" in codewars:
+#         for lines in codewars:
+#             print(lines)
+
+def codewars_command(exercises_db = "exercises.csv" , codewars_db = "codewars.csv" , processed_info = "processed.csv"):
+    if  os.path.isfile(exercises_db):
+        exercises = open(exercises_db , "r")
+    if  os.path.isfile(codewars_db):
+        codewars = open(codewars_db , "r")
+    if  os.path.isfile(processed_info):
+        with open(processed_info , "w") as processed:
+            processed.write("")
+        processed = open(processed_info , "a")
+    else:
+        processed = open(processed_info , "a")
+    print(exercises.closed,codewars.closed,processed.closed)
+codewars_command()
