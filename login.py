87e1058d73645599b3629aa967b0abd2b70d519f
ManuelSoto01 <37076222+ManuelSoto01@users.noreply.github.com>
2018-03-07 00:45:37 -0400

    Update login.py

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ def paubot()#Creamos una funcion, en la cual se creara los algoritmos para que d
   matricula = (input("Ingrese su matricula")) #Creamos una variable en la cual introducimos un input el cual mostrara que debemos ingresar para completar dicho paso.
   if len(matricula)==8 and matricula.isnumeric():#Aqui decimos que la variable debe tener una longitud de 8 digitos y debe ser numerica.
     return True #Asi retornamos que es verdadero cuando cumple cada paso correctamente.
-  else:
-    print ("matricula", matricula, "no es correcta")
+  else: 
+    print ("matricula", matricula, "no es correcta") #Y un else, que de lo contrario a lo planteado anterior, imprima que la matricula es incorrecta
 
 paubot()
